3da18a61799bd9923700eaab32cd02e06a5f5976
Seungwoo Hong <kilos.network@gmail.com>
2022-04-12 12:31:28 +0900

    fix: exit signal logic

--- src/yesno.js
+++ src/yesno.js
@@ -1,10 +1,10 @@
 const yesno = require('yesno');   // commonjs approach
 const onExit = require('signal-exit')
-
+let isSignal = true;
 onExit(function (code, signal) {
-  console.log('signal!')
-  console.log('\n-------STOP-------');
-  process.exit(1); // cntl-c 를 해도 exit(1)로 파이프 라인에서 인식되도록
+//   console.log('signal!', code, isSignal)
+//   console.log('\n-------STOP-------');
+  process.exit(!isSignal && code == 0? 0 : 1); // cntl-c 를 해도 exit(1)로 파이프 라인에서 인식되도록
 });
 
 (async () => {
@@ -15,10 +15,12 @@ onExit(function (code, signal) {
         noValues: [ 'n', 'no', 'N', 'No', 'NO', 'ㅜ' ],
     });
     if(ok) {
-        console.log('\n-------NEXT-------');
+        // console.log('\n-------NEXT-------');
+        isSignal = false;
         process.exit(0);
     } else {
-        console.log('\n-------STOP-------');
+        // console.log('\n-------STOP-------');
+        isSignal = false;
         process.exit(1); // Uncaught Fatal Exception https://nodejs.org/api/process.html#process_exit_codes
     }
 })();
